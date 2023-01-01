546dcb3a8a256c68034700ef1a35d4d6061f6645
Johan Hanses <johanhanses@gmail.com>
2023-01-01 19:56:46 +0100

    Move to components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import { motion } from 'framer-motion'
-import { ComponentPropsWithoutRef, useState } from 'react'
+import { useState } from 'react'
+import { Step } from '../components/Step'
 
 export default function Page() {
   let [step, setStep] = useState(1)
@@ -42,71 +42,5 @@ export default function Page() {
         </div>
       </div>
     </div>
-  )
-}
-
-function Step({ step, currentStep }: { step: number; currentStep: number }) {
-  const status = currentStep === step ? 'active' : currentStep < step ? 'inactive' : 'complete'
-
-  return (
-    <div className="relative">
-      <motion.div
-        animate={status}
-        variants={{
-          active: {
-            scale: 1,
-            transition: {
-              delay: 0,
-              duration: 0.2
-            }
-          },
-          complete: { scale: 1.25 }
-        }}
-        transition={{ duration: 0.6, delay: 0.2, type: 'tween', ease: 'circOut' }}
-        className="absolute inset-0 rounded-full bg-blue-200"
-      ></motion.div>
-      <motion.div
-        initial={false}
-        animate={status}
-        variants={{
-          inactive: {
-            backgroundColor: 'var(--white)',
-            borderColor: 'var(--slate-200)',
-            color: 'var(--slate-400)'
-          },
-          active: {
-            backgroundColor: 'var(--white)',
-            borderColor: 'var(--blue-500)',
-            color: 'var(--blue-500)'
-          },
-          complete: {
-            backgroundColor: 'var(--blue-500)',
-            borderColor: 'var(--blue-500)',
-            color: 'var(--blue-500)'
-          }
-        }}
-        transition={{ duration: 0.2 }}
-        className="relative flex h-10 w-10 items-center justify-center rounded-full border-2 font-semibold"
-      >
-        <div className="flex items-center justify-center">
-          {status === 'complete' ? <CheckIcon className="h-6 w-6 text-white" /> : <span>{step}</span>}
-        </div>
-      </motion.div>
-    </div>
-  )
-}
-
-function CheckIcon(props: ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg {...props} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={3}>
-      <motion.path
-        initial={{ pathLength: 0 }}
-        animate={{ pathLength: 1 }}
-        transition={{ delay: 0.2, type: 'tween', ease: 'easeOut', duration: 0.3 }}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M5 13l4 4L19 7"
-      />
-    </svg>
   )
 }
